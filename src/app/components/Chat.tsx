5d8244355b63e5809057a1bb8e283ce6d11fb3af
Ritesh <ritesh.paliwal@cssindiaonline.com>
2023-12-19 18:37:17 +0530

    Chat alignment issue

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -29,7 +29,7 @@ const Chat: React.FC = () => {
         <div className="p-3 flex flex-col gap-y-3" >
           {/* Check if activeVideoDetail and chat exist */}
           {activeVideoDetail && activeVideoDetail.messages && activeVideoDetail.messages.map((message:Message) => (
-            <div key={message.id} className={`flex ${message.sender === 'user' ? 'items-start' : 'items-end'}`}>
+            <div key={message.id} className={`flex items-center ${message.sender === 'user' ? ' justify-end' : ''}`}>
               <div className={`max-w-[90%] ${message.sender === 'user' ? 'bg-[#171717] text-white rounded-br-none' : 'bg-blue-500 text-white rounded-bl-none'} text-sm py-3 px-4 rounded-lg rounded-${message.sender === 'user' ? 'tl' : 'tr'}-none`}>
                 {message.text}
               </div>
