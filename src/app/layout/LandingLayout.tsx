c74d2559498886ce422e4b358bf84abb39c734b4
Ritesh <ritesh.paliwal@cssindiaonline.com>
2023-12-21 11:39:10 +0530

    mail form API correction

--- src/app/layout/LandingLayout.tsx
+++ src/app/layout/LandingLayout.tsx
@@ -4,6 +4,7 @@ import Header from "@/app/components/Header";
 import Footer from "@/app/components/Footer";
 import SignUp from "../components/SignUp";
 import { useState } from "react";
+import RequestDemo from "../components/RequestDemo";
 
 interface LandingLayoutProps {
   showButton?: boolean;
@@ -17,16 +18,22 @@ const LandingLayout: React.FC<LandingLayoutProps> = ({
   children,
 }) => {
   const [modalOpen, setOpenModal] = useState(false);
+  const [requestmodalOpen, requestsetOpenModal] = useState(false);
 
   const joinModalHandler = () => {
     setOpenModal(!modalOpen);
   };
 
+  const requestModalHandler = () => {
+    requestsetOpenModal(!requestmodalOpen);
+  };
+
   return (
     <>
-      <Header showButton={showButton} joinUSAction={joinModalHandler} />
+      <Header showButton={showButton} joinUSAction={joinModalHandler} requestAction={requestModalHandler} />
       {children}
       <SignUp popupAction={modalOpen} onCloseModal={joinModalHandler} />
+      <RequestDemo popupAction={requestmodalOpen} onCloseModal={requestModalHandler} />
       <Footer className={footerClass} />
     </>
   );
