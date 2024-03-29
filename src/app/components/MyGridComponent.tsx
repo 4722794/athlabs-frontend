6cbd8397082512b4244a8a9ea7e3b4ffb6a093c4
atifkhan2456 <atif.khan@cssindiaonline.com>
2024-03-29 09:27:51 +0530

    Leaderboard show public data and add pagination

--- src/app/components/MyGridComponent.tsx
+++ src/app/components/MyGridComponent.tsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 
 const MyGridComponent = ({ columnDefs, rowData }: any) => {
+  const defaultPageSize = 10; // Set the default page size
+
+  const gridApiRef = useRef<any>(null);
+
+  const gridOptions = {
+    pagination: true,
+    paginationPageSize: defaultPageSize,
+  };
+
+  function onGridReady(params: any) {
+    gridApiRef.current = params.api;
+  }
+
   return (
     <div
       className="ag-theme-alpine myAG_table"
       style={{ height: "470px", width: "100%" }}
     >
-      <AgGridReact columnDefs={columnDefs} rowData={rowData} />
+      <AgGridReact
+        columnDefs={columnDefs}
+        rowData={rowData}
+        gridOptions={gridOptions}
+        onGridReady={onGridReady}
+      />
     </div>
   );
 };
--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,46 +1,24 @@
 "use client";
+import { useEffect, useState } from "react";
 import MyGridComponent from "../components/MyGridComponent";
 import Tabs from "../components/Tabs";
 import LandingLayout from "../layout/LandingLayout";
 import { IoRefresh } from "react-icons/io5";
+import { callApi, checkLogin } from "../services/apiUtils";
 
 function Leaderboard() {
+  const [leaderboardData, setLeaderboardData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [loggedInUser, setLoggedInUser] = useState(null); // Assume this is set from an authentication service
+
   const footerClass = "!relative";
 
   const columnDefs = [
-    { headerName: "ID", field: "id", width: 90 },
+    // { headerName: "ID", field: "id", width: 90 },
     { headerName: "Name", field: "name", flex: 1 },
     { headerName: "Activity", field: "activity", flex: 1 },
     { headerName: "Score", field: "score", flex: 1 },
-  ];
-
-  const rowData = [
-    { id: 1, name: "Toyota", activity: "Celica", score: 35000 },
-    { id: 1, name: "Ford", activity: "Mondeo", score: 32000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
-    { id: 1, name: "Porsche", activity: "Boxster", score: 72000 },
   ];
 
   const tabsData = [
@@ -49,16 +27,86 @@ function Leaderboard() {
       label: "Public",
       content: (
         <div className=" text-white">
-          <MyGridComponent columnDefs={columnDefs} rowData={rowData} />
+          <MyGridComponent columnDefs={columnDefs} rowData={leaderboardData} />
         </div>
       ),
     },
-    {
-      id: "Personal",
-      label: "Personal",
-      content: <div className=" text-white">Content for Tab 2</div>,
-    },
+    // {
+    //   id: "Personal",
+    //   label: "Personal",
+    //   content: (
+    //     <div className=" text-white">
+    //       <MyGridComponent
+    //         columnDefs={columnDefs}
+    //         rowData={
+    //           loggedInUser
+    //             ? leaderboardData.filter(
+    //                 (entry) => entry.author === loggedInUser
+    //               )
+    //             : []
+    //         }
+    //       />
+    //     </div>
+    //   ),
+    // },
   ];
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiUrl = process.env.NEXT_PUBLIC_API_HOST;
+      const apiEndpoint = `${apiUrl}/leaderboard`;
+
+      try {
+        const response = await fetch(`${apiEndpoint}`, {
+          headers: {
+            accept: "application/json",
+            Authorization: `Bearer ${checkLogin()}`,
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const data = await response.json();
+        const sortedData = data.sort((a, b) => b.score - a.score);
+        // setLeaderboardData(sortedData.slice(0, 20)); // Only keep the top 20 entries
+        setLeaderboardData(sortedData);
+        setLoading(false);
+      } catch (error) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleRefresh = async () => {
+    setLoading(true); // Set loading state to true to indicate data is being fetched
+    const apiUrl = process.env.NEXT_PUBLIC_API_HOST;
+    const apiEndpoint = `${apiUrl}/leaderboard`;
+
+    try {
+      const response = await fetch(apiEndpoint, {
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${checkLogin()}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+      const sortedData = data.sort((a, b) => b.score - a.score);
+      setLeaderboardData(sortedData); // Set all the leaderboard data
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
 
   return (
     <LandingLayout showButton={false} footerClass={footerClass}>
@@ -67,12 +115,18 @@ function Leaderboard() {
           <div className=" landscape:lg:h-[calc(100vh-250px)]  landscape:lg:min-h-[calc(100vh-250px)]  min-h-[calc(100vh-250px)] lg:min-h-[422px]  lg:h-[calc(100vh-250px)] bg-[#1B212E]  rounded-xl w-full">
             <div className=" flex justify-between px-5 pt-3 items-center">
               <h3 className=" text-white text-2xl">Leaderboard</h3>
-              <button className=" text-white border px-5 py-2 rounded-3xl hover:bg-[#2F3747] hover:border-[#2F3747] inline-flex items-center">
+              <button
+                onClick={handleRefresh}
+                className=" text-white border px-5 py-2 rounded-3xl hover:bg-[#2F3747] hover:border-[#2F3747] inline-flex items-center"
+              >
                 <IoRefresh className="text-2xl mr-2" />
                 Refresh
               </button>
             </div>
             <Tabs tabs={tabsData} />
+
+            {loading && <div>Loading...</div>}
+            {error && <div>Error.</div>}
           </div>
         </div>
       </div>
