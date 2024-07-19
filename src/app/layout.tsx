968788b0f9b4ea7baac131ce67d6338995b0b257
Surojit-KB <bhattacharyya.surojit14995@gmail.com>
2024-07-19 16:39:51 +0530

    SEO update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,13 +6,13 @@ import "@/app/globals.css";
 import Main from "./layout/Main";
 
 export const metadata: Metadata = {
-  title: "Athlabs",
+  title: "Athlabs - AI Performance Coach",
   description: "Discover Athlabs, your personal AI coach for fitness training. Get real-time feedback on your workout form, tailored improvement suggestions, and insights to enhance your performance and safety.",
-  keywords: "fitness, AI, artificial intelligence, strength, conditioning, gym, online gym, workout, AI fitness coach, personal training app, fitness technology, exercise analytics, real-time workout feedback, health improvement, sports training assistant, fitness app, physical therapy, exercise form correction, football, cricket, tennis, basketball, weight, weightlifting, weight training, strength training, exercise",
+  keywords: "fitness, yoga, AI, artificial intelligence, strength, conditioning, gym, online gym, workout, AI fitness coach, personal training app, fitness technology, exercise analytics, real-time workout feedback, health improvement, sports training assistant, fitness app, physical therapy, exercise form correction, football, cricket, tennis, basketball, weight, weightlifting, weight training, strength training, exercise, form correction, right way to perform a deadlift",
   robots: "https://www.athlabs.co",
   openGraph: {
-    title: 'Athlabs',
-    description: 'Get personalized feedback on your exercise form with Athlabs.',
+    title: 'Athlabs - AI Performance Coach',
+    description: 'Get personalized feedback on your exercise and yoga form with Athlabs.',
     images: 'https://www.athlabs.co/images/logo2.png',
     type: 'website',
     siteName: 'Athlabs'
