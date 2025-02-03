d64eb7410fe20b02ac5a6607b69f7f69b25da103
hargun3045 <hargun3045@gmail.com>
2025-02-03 12:37:41 +0530

    deploy static page

--- next.config.js
+++ next.config.js
@@ -1,31 +1,33 @@
 /** @type {import('next').NextConfig} */
-const rewrites = () => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_HOST;
-  return [
-    {
-      source: "/video-upload",
-      destination: apiUrl,
-    },
-    {
-      source: "/token",
-      destination: apiUrl + "/token",
-    },
-    {
-      source: "/getVideoHistory",
-      destination: apiUrl,
-    },
-    {
-      source: "/getVideoDetailByVideoId/:id",
-      destination: apiUrl + "/h/:id",
-    },
-    {
-      source: "/processVideo",
-      destination: apiUrl + "/process",
-    },
-  ];
-};
+// Commenting out the rewrites so that it works on github
+// const rewrites = () => { 
+//   const apiUrl = process.env.NEXT_PUBLIC_API_HOST;
+//   return [
+//     {
+//       source: "/video-upload",
+//       destination: apiUrl,
+//     },
+//     {
+//       source: "/token",
+//       destination: apiUrl + "/token",
+//     },
+//     {
+//       source: "/getVideoHistory",
+//       destination: apiUrl,
+//     },
+//     {
+//       source: "/getVideoDetailByVideoId/:id",
+//       destination: apiUrl + "/h/:id",
+//     },
+//     {
+//       source: "/processVideo",
+//       destination: apiUrl + "/process",
+//     },
+//   ];
+// };
 const nextConfig = {
-  rewrites,
+  // rewrites,
+  output: "export",
 };
 
 module.exports = nextConfig;
